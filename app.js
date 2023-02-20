00061cd8b4524335de51bee6d6997a3530d0ab15
SohaibIbenhajene <r0883629@student.thomasmore.be>
2023-02-20 02:25:18 +0100

    Biggest portion of my portfolio is done.

---
+++ app.js
@@ -0,0 +1,46 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".header .nav-bar .nav-list ul li");
+
+window.addEventListener("scroll", () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop - sectionHeight / 3)){
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach( li => {
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active');
+        }
+    });
+});
